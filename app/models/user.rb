663a792a279e8bbe83a3edce1e2a68a02c02493f
jbudrow <jbudrowd3@yahoo.com>
2016-04-16 09:10:58 -0400

    validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name, :email, :ssn, :dob
+  validates :password, length: { minimum: 8 }
+  validates :email, format: {
+    with: /.+@.+\..+/, messages: 'Invalid email'
+  }
+  validates :ssn, format: {
+    with: ^(\d{3}-?\d{2}-?\d{4}|XXX-XX-XXXX)$
+  }
+
+  #TODO regex validaton for Date of Birth.
 end
