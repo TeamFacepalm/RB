53a88a869d2b64d229393a2b316a952b1e048c37
jbudrow <jbudrowd3@yahoo.com>
2016-04-16 11:55:00 -0400

    create sessions controller, with two controller methods sessions create/check_auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  before_action :authenticate!, only: [:check_auth]
+
+  def create
+    @user = User.find_by!(email: params['email'])
+    if @user.authenticate(params['password'])
+      render json: { user: @user.as_json(only: [:email, :auth_token]) },
+             status: :ok
+    else
+      render json: { message: 'Invalid email or password.' },
+             status: :unauthorized
+    end
+  end
+
+  def check_auth
+    if logged_in_user
+      render json: { message: 'Successfully logged in #{logged_in_user.email}.' },
+             status: :ok
+    else
+      render json: { message: 'Invalid authorization' },
+             status: :unauthorized
+    end
+  end
+end
+        
