62f1f11cb34d8c272496a65e30f93fad90b896a5
slatersan1 <cdlee212@gmail.com>
2016-04-16 11:36:58 -0400

    add create method to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(first_name: params["first_name"],
+                     last_name: params["last_name"],
+                     email: params["email"],
+                     ssn: params["ssn"],
+                     dob: params["dob"], 
+                     avatar: params["avatar"],
+                     district_id: params["district_id"])
+
+    @user.ensure_auth_token  
+    if @user.save
+      render "create.json.jbuilder", status: :created
+           
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
 end
