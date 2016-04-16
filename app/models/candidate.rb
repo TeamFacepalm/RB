82e20da8ce0a4153cb00521d8924656048b5b4e3
jbudrow <jbudrowd3@yahoo.com>
2016-04-16 11:10:29 -0400

    add candidate/district associations

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,5 @@
 class Candidate < ActiveRecord::Base
+  belongs_to :district
+  has_many :votes
+  has_many :images
 end
--- app/models/district.rb
+++ app/models/district.rb
@@ -1,2 +1,5 @@
 class District < ActiveRecord::Base
+  has_many :candidates
+  has_many :images
+  has_many :users
 end
