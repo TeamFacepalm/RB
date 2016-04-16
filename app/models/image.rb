033fc80fd15ffa7b19be623340fbaa389862ec47
jbudrow <jbudrowd3@yahoo.com>
2016-04-16 11:18:19 -0400

    validations for image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,10 @@
 class Image < ActiveRecord::Base
   belongs_to :candidate
   belongs_to :user
-  belongs_to :district 
+  belongs_to :district
+
+  has_attached_file :avatar
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
+  validates_presence_of :user_id, :district_id, :candidate_id
 end
