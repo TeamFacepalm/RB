7a45e81df27fe59d017d6aa343dfa91ffb3f5133
jbudrow <jbudrowd3@yahoo.com>
2016-04-17 12:30:37 -0400

    comment out user model auth_token validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
 
-  validates :auth_token, presence: true
+  # validates :auth_token, presence: true
   validates_presence_of :first_name, :last_name, :email, :ssn, :dob
   validates :password, length: { minimum: 3 }
   validates :email, format: {
